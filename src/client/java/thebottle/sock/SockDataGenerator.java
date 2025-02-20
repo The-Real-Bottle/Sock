6998ffe023bb415b84543652c44e777aa14e74d0
KrazyMiner001 <krazyminer001@outlook.com>
2025-02-20 17:40:27 +0200

    feat(Client): Added model datagen

--- src/client/java/thebottle/sock/SockDataGenerator.java
+++ src/client/java/thebottle/sock/SockDataGenerator.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.data.DataGenerator;
 import thebottle.sock.datagen.providers.SockEquipmentAssetProvider;
+import thebottle.sock.datagen.providers.SockModelProvider;
 import thebottle.sock.datagen.providers.languages.EnUSLangProvider;
 
 public class SockDataGenerator implements DataGeneratorEntrypoint {
@@ -11,6 +12,7 @@ public class SockDataGenerator implements DataGeneratorEntrypoint {
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 		pack.addProvider(EnUSLangProvider::new);
+		pack.addProvider(SockModelProvider::new);
 
 		DataGenerator.Pack vanillaPack = fabricDataGenerator.createPack();
 		vanillaPack.addProvider(SockEquipmentAssetProvider::new);
