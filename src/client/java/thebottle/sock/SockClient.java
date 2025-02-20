d5b24c15e36331ee79bb081c750b1342a0158ee5
KrazyMiner001 <krazyminer001@outlook.com>
2025-02-20 17:33:04 +0200

    feat(Client): Added datagen for equipment assets

--- src/client/java/thebottle/sock/SockClient.java
+++ src/client/java/thebottle/sock/SockClient.java
@@ -1,10 +1,25 @@
 package thebottle.sock;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.MinecraftClient;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SockClient implements ClientModInitializer {
+	private static final List<String> messages;
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		Sock.LOGGER.info(messages.getFirst());
+	}
+
+	static {
+		//Choose a random message
+		messages = new ArrayList<>();
+		messages.add("Hello");
+		Collections.shuffle(messages);
 	}
 }
--- src/client/java/thebottle/sock/SockDataGenerator.java
+++ src/client/java/thebottle/sock/SockDataGenerator.java
@@ -2,10 +2,17 @@ package thebottle.sock;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.data.DataGenerator;
+import thebottle.sock.datagen.providers.SockEquipmentAssetProvider;
 
 public class SockDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		//Add datagens that don't need bootstaps using pack
+
+		DataGenerator.Pack vanillaPack = fabricDataGenerator.createPack();
+		vanillaPack.addProvider(SockEquipmentAssetProvider::new);
 	}
 }
