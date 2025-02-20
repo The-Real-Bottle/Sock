f3cc58bb741458e878e703a561210f7e695db2cf
KrazyMiner001 <krazyminer001@outlook.com>
2025-02-20 17:37:08 +0200

    feat(Client): Added language datagen

--- src/client/java/thebottle/sock/SockDataGenerator.java
+++ src/client/java/thebottle/sock/SockDataGenerator.java
@@ -4,13 +4,13 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.data.DataGenerator;
 import thebottle.sock.datagen.providers.SockEquipmentAssetProvider;
+import thebottle.sock.datagen.providers.languages.EnUSLangProvider;
 
 public class SockDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-
-		//Add datagens that don't need bootstaps using pack
+		pack.addProvider(EnUSLangProvider::new);
 
 		DataGenerator.Pack vanillaPack = fabricDataGenerator.createPack();
 		vanillaPack.addProvider(SockEquipmentAssetProvider::new);
